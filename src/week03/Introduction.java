1d0b98b66d20e7c4411ccd3e62f92756e16ef93c
Guilherme <frias.guilherme@aluno.ifsp.edu.br>
2023-03-07 17:49:58 -0300

    week 3 - types

---
+++ src/week03/Introduction.java
@@ -0,0 +1,34 @@
+package week03;
+
+public class Introduction {
+    public static void main(String[] args) {
+        //One line comment
+        /*Multi line comment*/
+
+        //primitive Types
+
+        // Integers - byte, shot, int, long
+        byte nByte = 100; // 1 byte - -128 to 127
+        short nShort = 3000; // 2 bytes - -32768 to 32767
+        int nInt = 20000000; // 4 bytes - 2 billions
+        long nLong = 323232323232L; //8 bytes
+
+        //Floating point
+        float nFloat = 10.34343f;
+        double nDouble = 10.3434343;
+
+        //Char
+        char letter = 'A';
+
+        //Boolean
+        boolean trueOrFalse = true;
+
+        //No primitive
+
+        // String
+        String name = "Guilherme Frias";
+        System.out.println(name);
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+    } 
+}
